829de69a3cb3b258d55ceede0f805fe0faca4ff8
SoeRatch <surajkoho@gmail.com>
2019-02-23 05:33:55 +0530

    Updates

---
+++ precache-manifest.3be642b826926947339aef1edb44af36.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdd68d2a3fac5450845966a25b410449",
+    "url": "/portfolio/static/media/Restofants.fdd68d2a.png"
+  },
+  {
+    "revision": "bb5b21db39e0174ac1e5",
+    "url": "/portfolio/static/css/main.d5bc73b0.chunk.css"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "4cf5d245443d5126aab4",
+    "url": "/portfolio/static/js/2.4cf5d245.chunk.js"
+  },
+  {
+    "revision": "c1a95f7335cbfb730b937e19e8830a53",
+    "url": "/portfolio/static/media/programming.c1a95f73.svg"
+  },
+  {
+    "revision": "9468d1fe315606e4464cfb43fb508249",
+    "url": "/portfolio/static/media/Pensheel.9468d1fe.png"
+  },
+  {
+    "revision": "bb5b21db39e0174ac1e5",
+    "url": "/portfolio/static/js/main.bb5b21db.chunk.js"
+  },
+  {
+    "revision": "c2f580f35002b7919d1a8ffb9b1dcf83",
+    "url": "/portfolio/static/media/stockcharts.c2f580f3.png"
+  },
+  {
+    "revision": "51345c9d448cdc0356583b453c4e3243",
+    "url": "/portfolio/static/media/ifloopz.51345c9d.png"
+  },
+  {
+    "revision": "bfeea82dfac6f6180e30776bf062bbf8",
+    "url": "/portfolio/static/media/jobaaks.bfeea82d.png"
+  },
+  {
+    "revision": "9b269fe2e20a99cc9d05dd5722cfb090",
+    "url": "/portfolio/static/media/conversation.9b269fe2.svg"
+  },
+  {
+    "revision": "abc35b5da0ead80df7ce044a1373b71e",
+    "url": "/portfolio/index.html"
+  }
+];
