4256c48bc570f68889f30012689df913e59aae3a
SoeRatch <surajkoho@gmail.com>
2019-03-06 22:43:45 +0530

    Updates

--- precache-manifest.7c2fe18dde94f5fd6bb90cff65cdbefb.js
+++ precache-manifest.7c2fe18dde94f5fd6bb90cff65cdbefb.js
@@ -4,7 +4,7 @@ self.__precacheManifest = [
     "url": "/portfolio/static/media/Restofants.fdd68d2a.png"
   },
   {
-    "revision": "01962df3ff2833d8961a",
+    "revision": "2fe45e3180d69905c251",
     "url": "/portfolio/static/css/main.d5bc73b0.chunk.css"
   },
   {
@@ -24,8 +24,8 @@ self.__precacheManifest = [
     "url": "/portfolio/static/media/Pensheel.9468d1fe.png"
   },
   {
-    "revision": "01962df3ff2833d8961a",
-    "url": "/portfolio/static/js/main.01962df3.chunk.js"
+    "revision": "2fe45e3180d69905c251",
+    "url": "/portfolio/static/js/main.2fe45e31.chunk.js"
   },
   {
     "revision": "c2f580f35002b7919d1a8ffb9b1dcf83",
@@ -44,7 +44,7 @@ self.__precacheManifest = [
     "url": "/portfolio/static/media/conversation.9b269fe2.svg"
   },
   {
-    "revision": "00e98977456fecadce461912b0da1999",
+    "revision": "bd07f6c51c49fa3a622b64e4eae5719f",
     "url": "/portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.ae7ddc5c685cedf09a7ca24399cd2bbd.js"
+  "/portfolio/precache-manifest.7c2fe18dde94f5fd6bb90cff65cdbefb.js"
 );
 
 workbox.clientsClaim();
